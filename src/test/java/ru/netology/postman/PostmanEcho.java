ff2465078065142efec0c1993d06ed0daaacd408
vladkor888 <lidnn97_1997@mail.ru>
2022-06-29 17:33:07 +0300

    Success test

--- src/test/java/ru/netology/postman/PostmanEcho.java
+++ src/test/java/ru/netology/postman/PostmanEcho.java
@@ -19,7 +19,7 @@ public class PostmanEcho {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("ПО"))
+                .body("data", equalTo("Тестирование"))
         ;
     }
 
